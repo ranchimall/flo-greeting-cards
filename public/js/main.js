6eacf4835ad41283ad627de6365ee54817150abb
Abhishek Sinha <avishkarabhishek786@gmail.com>
2018-07-31 22:08:26 +0530

    improved pdf generation functionality, dynamic fields on cards

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,39 @@
+const html2canvas = require('html2canvas');
+
+$(document).on('keyup', '.stick_text', function() {
+  var btn = this;
+  var _id = $(btn).attr('id');
+  
+  if(_id == '_to') {
+    var to = $(btn).val();
+    $("#span_to").text(to);
+  } else if(_id == '_from') {
+    var from = $(btn).val();
+    $("#span_from").text(from);
+  } else if(_id == '_bdata') {
+    var usg_msg = $(btn).val();
+    $("#span_user_msg").text(usg_msg);
+  }
+});
+
 $(document).on('click', '#bc-btn', function() {
   var text = $('#_bdata').val();
+  var _from = $('#_from').val();
+  var _to = $('#_to').val();
   var _cardid = $('#_cardid').val();
     $.ajax({
       type: 'post',
       url: '/write',
-      data: {_bdata:text, _cardid:_cardid},
+      data: {_bdata:text,_from:_from, _to:_to, _cardid:_cardid},
       success: function(data) {
-        console.log(data);
-        //console.log(JSON.parse(data));  
-        
-        var doc = new jsPDF()
-        var specialElementHandlers = {
-          '#pdfignore': function(element, renderer){
-            return true;
-          }
-        };
-        //console.log($('#pdfcontent').html());
-        
-        doc.fromHTML($('#pdfcontent').html(), 15, 15, {
-          'width': 170, 
-          'elementHandlers': specialElementHandlers
-        },
-        function(bla){doc.save('card.pdf');},
-        doc.autoPrint()
-      );
+
+        html2canvas(document.getElementById("pdfcontent"), { allowTaint: true }).then(function(canvas) {
+            var img = canvas.toDataURL("image/png");
+            var doc = new jsPDF('p', 'mm', 'a3');
+            doc.addImage(img, 'PNG', 10, 10);
+            doc.save("test.pdf");
+            doc.autoPrint();
+        });
 
       },
       error: function(jqXHR, textStatus, errorThrown) {
--- routes.js
+++ routes.js
@@ -27,13 +27,25 @@ router.get('/write/:card_id', (req, res)=>{
 })
 
 router.post('/write', [
+  check('_from')
+    .isLength({min:1})
+    .isAlpha()
+    .withMessage('Please specify the name of the sender!')
+    .trim(),
+  check('_to')
+    .isLength({min:1})
+    .isAlpha()
+    .withMessage('Please specify the name of the receiver!')
+    .trim(),
   check('_bdata')
     .isLength({min:1})
-    .withMessage('Please write some remarks!')
+    .isAlphanumeric()
+    .withMessage('Please write the message to be sent to receiver!')
     .trim(),
   check('_cardid')
     .isLength({min:1})
-    .withMessage('Could not find the card id!')
+    .isAlphanumeric()
+    .withMessage('Could not find the card id! Please select the card again.')
     .trim()
 ],
   (req,res)=>{
@@ -49,20 +61,23 @@ router.post('/write', [
     const data = matchedData(req)
 
     let user_msg = data._bdata;
+    let _from = data._from;
+    let _to = data._to;
     let card_id = data._cardid;
 
     var toaddress = "oXCsMUyX3mLJEdnn8SXoH6gyPW9Jd6kjYu";
     var amount = 1;
     
     try {
-        client.sendToAddress(toaddress, amount, "Greetings App", "REBC", false, false, 1, 'UNSET', user_msg)
-        .then((txnid) => {
-          console.log(txnid)
-          res.json({"error":false, "txnid":txnid, "card_id":card_id, "user_msg":user_msg})
-        });
+        // client.sendToAddress(toaddress, amount, "Greetings App", "REBC Greetigs App", false, false, 1, 'UNSET', user_msg)
+        // .then((txnid) => {
+        //   console.log(txnid)
+        //   res.json({"error":false, "txnid":txnid, "_from":_from, "_to":_to, "card_id":card_id, "user_msg":user_msg})
+        // });
+        res.json({"error":false, "txnid":"TXIDKJKLGJLKSJLKGJSKJGK", "_from":_from, "_to":_to, "card_id":card_id, "user_msg":user_msg})
     }catch(err){
         console.log("Unable to send FLO." + err.message);
-    }
+    } 
 
     //res.json({"error":true, "txnid":null, "card_id":null, "user_msg":null})
 
